4002d0ddcdedc174105ea5a6509b7497a5dd8654
joenash156 <joenash156@gmail.com>
2026-03-25 09:42:30 +0000

    add eas.json configuration and update app.json; comment out unused imports in UserLayout

--- frontend/app/user/_layout.tsx
+++ frontend/app/user/_layout.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+// import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
 
@@ -6,7 +6,7 @@ const UserLayout = () => {
   return (
     <Stack
       screenOptions={{
-        headerShown: false
+        headerShown: false,
       }}
     />
   )
